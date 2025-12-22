a60683b06f85f047705ce80aacd5e2d0f8f303fe
jomrembau <jomir.bautista@gmail.com>
2025-12-22 17:40:02 +0100

    Add files via upload

---
+++ Who is buying lunch.js
@@ -0,0 +1,4 @@
+var nameList = ["Angela", "Ben", "Jenny", "Michael", "Chloe"];
+var randomNumber = Math.floor(Math.random() * nameList.length);
+
+alert(nameList[randomNumber] + " is going to buy lunch today.")
