f4fc478a83b2d33f5171a0be972ef859b3b284f6
jomrembau <jomir.bautista@gmail.com>
2025-12-23 14:44:19 +0100

    Fix output for the last bottle in the song

--- 99 Bottles of Beer on the wall.js
+++ 99 Bottles of Beer on the wall.js
@@ -5,7 +5,8 @@ while (numberOfBottles > 1) {
     numberOfBottles --;
     console.log("Take one down and pass it around, " + numberOfBottles + " bottles of beer on the wall.\n");
 }
-console.log("1 more bottle of beer on the wall, no more bottles of beer.")
+console.log("1 more bottle of beer on the wall, 1 more bottle of beer.")
 console.log("Take one down and pass it around, no more bottles of beer on the wall.\n")
 console.log("No more bottles of beer on the wall, no more bottles of beer.")
 console.log("Go to the store and buy some more, 99 bottles of beer on the wall.")
+
