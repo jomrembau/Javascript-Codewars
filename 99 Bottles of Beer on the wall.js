8df80f5f0a631f0ce700dc91148bc82a806baee6
jomrembau <jomir.bautista@gmail.com>
2025-12-23 14:43:16 +0100

    Refactor '99 Bottles of Beer' logic for singular case
    Modified the loop to handle the case for 1 bottle and added final output for 1 bottle.

--- 99 Bottles of Beer on the wall.js
+++ 99 Bottles of Beer on the wall.js
@@ -1,10 +1,11 @@
 var numberOfBottles = 99;
 
-while (numberOfBottles > 0) {
+while (numberOfBottles > 1) {
     console.log(numberOfBottles + " bottles of beer on the wall, " + numberOfBottles + " bottles of beer.");
     numberOfBottles --;
     console.log("Take one down and pass it around, " + numberOfBottles + " bottles of beer on the wall.\n");
 }
-
+console.log("1 more bottle of beer on the wall, no more bottles of beer.")
+console.log("Take one down and pass it around, no more bottles of beer on the wall.\n")
 console.log("No more bottles of beer on the wall, no more bottles of beer.")
 console.log("Go to the store and buy some more, 99 bottles of beer on the wall.")
