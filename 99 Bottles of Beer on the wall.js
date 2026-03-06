0c383ba7c592a4f388f6e1650b19fbf513668ca1
jomrembau <jomir.bautista@gmail.com>
2026-03-06 19:14:26 +0100

    Refactor and format '99 Bottles of Beer' script

--- 99 Bottles of Beer on the wall.js
+++ 99 Bottles of Beer on the wall.js
@@ -8,5 +8,6 @@ while (numberOfBottles > 1) {
 console.log("1 bottle of beer on the wall, 1 bottle of beer.");
 console.log("Take one down and pass it around, no more bottles of beer on the wall.\n");
 console.log("No more bottles of beer on the wall, no more bottles of beer.");
-console.log("Go to the store and buy some more, 99 bottles of beer on the wall.");
+console.log("Go to the store and buy some more, 99 bottles of beer on the wall."); 
+
 
